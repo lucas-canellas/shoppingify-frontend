d7023057992f76df18bc7e17aee2441b8b8774e3
Lucas David <lucascanellasdev@gmail.com>
2023-02-03 13:42:27 -0300

    deploy

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL:
-    "http://shoppingify-env.eba-dhpk6cyj.us-east-1.elasticbeanstalk.com/",
+    "https://cors-everywhere.herokuapp.com/http://shoppingify-env.eba-dhpk6cyj.us-east-1.elasticbeanstalk.com/",
 });
 
 
