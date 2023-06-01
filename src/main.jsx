03e568e20ec12c326646e1966ff63858706f3040
Lucas David <lucascanellasdev@gmail.com>
2023-06-01 10:28:22 -0300

    404

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import { Home } from './pages/Home';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { MyProvider } from './context/Context';
+import './index.css';
 import { History } from './pages/History';
 import { HistoryDetails } from './pages/HistoryDetails';
-import { MyProvider } from './context/Context';
+import { Home } from './pages/Home';
 import { SignUp } from './pages/SignUp';
 import { Statistics } from './pages/Statistics';
 
@@ -29,6 +29,10 @@ const router = createBrowserRouter([
   {
     path: "/statistic",
     element: <Statistics />,
+  },
+  {
+    path: "*",
+    element: <Home />,
   }
 ]);
 
