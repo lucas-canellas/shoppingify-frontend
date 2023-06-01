2ed42ed31ef0ffd999aaca5c6797ade3ddc32dd2
Lucas David <lucascanellasdev@gmail.com>
2023-06-01 14:24:09 -0300

    rewrite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
-import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), pluginRewriteAll()],
+  plugins: [react(), svgr()],
   appType: 'react',
 })
