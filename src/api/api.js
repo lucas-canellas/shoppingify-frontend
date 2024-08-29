b81db61b06b9972ca1f9b67d2ecae90c6a131e67
Lucas Canellas <lucascanellas@id.uff.br>
2024-08-29 14:48:18 -0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL:
-    "http://vps54556.publiccloud.com.br:8080",
+    "https://shoppingify-api-production.up.railway.app",
 });
 
 
