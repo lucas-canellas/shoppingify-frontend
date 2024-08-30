ff1c6df4fb6966ec46434f2a8b5ea9374a559640
lucasdc <lucascanellasdev@gmail.com>
2024-08-30 11:55:07 -0300

    up

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL:
-    "shoppingify-api-production.up.railway.app",
+    "https://shoppingify-api-production.up.railway.app",
 });
 
 
